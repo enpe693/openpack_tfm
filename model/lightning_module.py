306998ceadd07656b81c86a5280e623a1c03ae1a
enpe693 <enpe693@gmail.com>
2023-08-08 00:08:06 +0200

    lightning module and models added

---
+++ model/lightning_module.py
@@ -0,0 +1,72 @@
+import os
+import shutil
+import logging
+from pathlib import Path
+from typing import Dict, Optional
+
+
+import hydra
+import numpy as np
+import openpack_toolkit as optk
+import openpack_torch as optorch
+import pandas as pd
+import pytorch_lightning as pl
+import torch
+from torch import nn
+from scipy.special import softmax
+from models import DeepConvLstmV3
+import matplotlib.pyplot as plt
+import seaborn as sns
+from hydra import initialize_config_dir, compose
+from omegaconf import DictConfig, OmegaConf
+
+from openpack_toolkit import OPENPACK_OPERATIONS
+
+class MyModelLM(optorch.lightning.BaseLightningModule):
+
+    def init_model(self, cfg: DictConfig) -> torch.nn.Module:
+        """ Initalize your model
+        
+        Generate an instance of the model you defined in the above.
+        Uncomment the target model.
+        """
+        # !! Edit Here !!
+        # NOTE: Please select the model you want to use!
+        #model = DeepConvLstmV1()
+        # model = DeepConvLstm()
+        model = DeepConvLstmV3()
+        return model
+    
+    
+    def init_criterion(self, cfg: DictConfig):
+        """Initialize loss function
+        """
+        ignore_cls = [(i, c) for i, c in enumerate(cfg.dataset.classes.classes) if c.is_ignore]
+        
+        criterion = torch.nn.CrossEntropyLoss(
+            ignore_index=ignore_cls[-1][0]
+        )
+        return criterion
+
+    def training_step(self, batch: Dict, batch_idx: int) -> Dict:
+        """Definition of training loop. Get mini-batch and return loss.
+        """
+        x = batch["x"].to(device=self.device, dtype=torch.float)
+        t = batch["t"].to(device=self.device, dtype=torch.long)
+        y_hat = self(x).squeeze(3)
+
+        loss = self.criterion(y_hat, t)
+        acc = self.calc_accuracy(y_hat, t)
+        return {"loss": loss, "acc": acc}
+
+    def test_step(self, batch: Dict, batch_idx: int) -> Dict:
+        """Definition of inference step. Get mini-batch and return model outputs.
+        """
+        x = batch["x"].to(device=self.device, dtype=torch.float)
+        t = batch["t"].to(device=self.device, dtype=torch.long)
+        ts_unix = batch["ts"]
+
+        y_hat = self(x).squeeze(3)
+
+        outputs = dict(t=t, y=y_hat, unixtime=ts_unix)
+        return outputs
