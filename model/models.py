cd94174c26575075746cde13b9e54880b205affe
enpe693 <enpe693@gmail.com>
2023-08-17 16:20:03 +0200

    model

--- model/models.py
+++ model/models.py
@@ -5,7 +5,7 @@ Reference:
 import torch
 from torch import nn
 from openpack_toolkit import OPENPACK_OPERATIONS
-
+import math
 
 class DeepConvLSTM(nn.Module):
     """Imprementation of DeepConvLSTM [Sensors 2016].
@@ -222,47 +222,38 @@ class DeepConvLstmV3(nn.Module):
         # -- [3] Output --
         x = self.out8(x)
         return x
-    
 
-class ModalityFusionModel(nn.Module):
+class CSNetWithFusion(nn.Module):
     def __init__(self, in_ch: int = 46, num_classes: int = None):
         super().__init__()
         if num_classes is None:
             num_classes = len(OPENPACK_OPERATIONS)
-
-        # -- [1] CNN --
-        # *** Edit Here ***
-        num_conv_layers = 4 # convolutional layers (Default: 4)
-        num_conv_filter = 64 # convolutional filters (Default: 64)
-        ks = 5 # kernel size, 
-        # ******************
         
-        blocks = []
-        for i in range(num_conv_layers):
-            in_ch_ = in_ch if i == 0 else 64
-            blocks.append(
-                nn.Sequential(
-                    nn.Conv2d(in_ch_, 64, kernel_size=(5, 1), padding=(2, 0)),
-                    nn.BatchNorm2d(64),
-                    nn.ReLU(),
-                )
-            )
-        self.conv_blocks = nn.ModuleList(blocks)
-
-        # -- [2] LSTM --
-        # *** Edit Here ***        
-        hidden_units = 128 # number of hidden units for Bi-LSTM
-        # ******************
+        self.imu_block = CSNetBlock(in_ch=12,num_classes=num_classes)
+        self.keypoints_block = CSNetBlock(in_ch=17, num_classes=num_classes)
+        self.e4_block =  CSNetBlock(in_ch=12, num_classes=num_classes)
         
-        # NOTE: enable ``bidirectional``
-        self.lstm6 = nn.LSTM(num_conv_filter, hidden_units, batch_first=True, bidirectional=True)
-        self.lstm7 = nn.LSTM(hidden_units*2, hidden_units, batch_first=True,  bidirectional=True)
-        self.dropout6 = nn.Dropout(p=0.5)
-        self.dropout7 = nn.Dropout(p=0.5)
+        
 
-        # -- [3] Output --
-        self.out8 = nn.Conv2d(
-            hidden_units * 2,
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+       
+        
+        return x
+    
+
+class CSNetBlock(nn.Module):
+    def __init__(self, in_ch: int = 46, num_classes: int = None):
+        super().__init__()
+        if num_classes is None:
+            num_classes = len(OPENPACK_OPERATIONS)
+        
+        self.conv1 = ConvolutionBlock(in_ch,num_classes)
+        self.pos = PositionalEncoding(64)
+        self.attn1 = SelfAttentionBlock()
+        self.attn2 = SelfAttentionBlock()
+        self.conv2 = ConvolutionBlock()
+        self.out = nn.Conv2d(
+            64,
             num_classes,
             1,
             stride=1,
@@ -270,34 +261,17 @@ class ModalityFusionModel(nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Args:
-            x (torch.Tensor): shape = (B, CH, T, 1)
-        Returns:
-            torch.Tensor: shape = (B, N_CLASSES, T, 1)
-        """
-        # -- [1] Conv --
-        for block in self.conv_blocks:
-            x = block(x)
-
-        # -- [2] LSTM --
-        # Reshape: (B, CH, 1, T) -> (B, T, CH)
-        x = x.squeeze(3).transpose(1, 2)
-
-        x, _ = self.lstm6(x)
-        x = self.dropout6(x)
-        x, _ = self.lstm7(x)
-        x = self.dropout7(x)
-
-        # Reshape: (B, T, CH) -> (B, CH, T, 1)
-        x = x.transpose(1, 2).unsqueeze(3) 
-        
-        # -- [3] Output --
-        x = self.out8(x)
+       
+        x = self.conv1(x)
+        x = self.pos(x)
+        x = self.attn1(x)
+        x = self.attn2(x)
+        x = self.conv2(x)
+        x = self.out(x)
         return x
     
 class ConvolutionBlock(nn.Module):
-    def __init__(self, in_ch: int = 1, num_classes: int = None, num_layers: int = 2, k: int = 3, filters = []):
+    def __init__(self, in_ch: int = 1, num_classes: int = None, num_layers: int = 2, k: int = 3, filters = [32,64]):
         super().__init__()
         if num_classes is None:
             num_classes = len(OPENPACK_OPERATIONS)
@@ -323,7 +297,7 @@ class ConvolutionBlock(nn.Module):
         return x
 
 class SelfAttentionBlock(nn.Module):
-    def __init__(self, embed_size, heads, dropout=0.1):
+    def __init__(self, embed_size=64, heads=8, dropout=0.1):
         super(SelfAttentionBlock, self).__init__()
         
         self.norm1 = nn.LayerNorm(embed_size)
@@ -351,3 +325,24 @@ class SelfAttentionBlock(nn.Module):
         x = residual + self.dropout(feed_forward_output)
         
         return x
+    
+class PositionalEncoding(nn.Module):
+#https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
